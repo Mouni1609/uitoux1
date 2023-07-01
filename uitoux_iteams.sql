e0af2e43bd56762f35b8a814d09db40f0ebf6054
Mouni1609 <136976336+Mouni1609@users.noreply.github.com>
2023-07-01 18:30:13 +0530

    Add files via upload

---
+++ uitoux_iteams.sql
@@ -0,0 +1,7 @@
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+flush privileges;
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER root IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+FLUSH PRIVILEGES;
